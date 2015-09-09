d541d8e01ba8048231168b661af4c883a3963c2b
Dominik Chmaj <dominik.chmaj@gmail.com>
2015-09-09 13:09:04 +0200

    Initial variable null
    Signed-off-by: Dominik Chmaj <dominik.chmaj@gmail.com>

--- lib/hooks/view_highlighted_hook.rb
+++ lib/hooks/view_highlighted_hook.rb
@@ -3,6 +3,7 @@ module Highlightjs
     class ViewHighlightedHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
         setting = Setting.plugin_redmine_highlightjs[:theme]
+        setting = 'monokai_sublime' if setting.nil? || setting.empty?
         return stylesheet_link_tag("#{setting}.css", :plugin => "redmine_highlightjs", :media => "screen") +
         stylesheet_link_tag("fixes.css", :plugin => "redmine_highlightjs", :media => "screen") +
         javascript_include_tag('highlight.pack.min.js', :plugin => 'redmine_highlightjs') +
