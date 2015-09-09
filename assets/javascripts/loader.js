9363fd9f91e1b295b0fdeb26293f49f3b786c267
Dominik Chmaj <dominik.chmaj@gmail.com>
2015-09-09 16:01:00 +0200

    task #1: Better performance and look for blocks with nohighlight tag
    Task-Url: http://github.com/dominch/redmine_highlightjs/issues/issue/1
    Signed-off-by: Dominik Chmaj <dominik.chmaj@gmail.com>

--- assets/javascripts/loader.js
+++ assets/javascripts/loader.js
@@ -1,8 +1,10 @@
 function updateCodes() {
     $('pre code:not(.hljs)').each(function(i, block) {
-        $(this).html($.trim($(this).html()));
-        hljs.highlightBlock($(this)[0]);
-        $(this).parent().show();
+        if (!$(this).hasClass('nohighlight')) { 
+            $(this).html($.trim($(this).html()));
+            hljs.highlightBlock($(this)[0]);
+            $(this).parent().show();
+        }
     });
     $('pre:not(:has(code))').each(function(i, block) {
         $(this).html($.trim($(this).html()));
