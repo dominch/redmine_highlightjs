5c4782feee00551e7b53804a8aff6058c3450749
Dominik Chmaj <dominik.chmaj@gmail.com>
2015-09-12 15:16:15 +0200

    task #3: No highlight in view-source mode
    Task-Url: http://github.com/dominch/redmine_highlightjs/issues/issue/3
    Signed-off-by: Dominik Chmaj <dominik.chmaj@gmail.com>

--- assets/javascripts/loader.js
+++ assets/javascripts/loader.js
@@ -1,15 +1,38 @@
 function updateCodes() {
+    $('table.syntaxhl th.line-num').remove();
+    $('table.syntaxhl').each(
+            function(i, block) {
+                var parentBlock = $(this).parent()
+                var code = '';
+                var line = 0;
+                $(this).find('td pre').each(
+                        function(i, block) {
+                            line++;
+
+                            var html = $(this).html()
+                            if (!html.trim()) html = "\n"
+                            code += html
+//                            code += '<span class="lineNumber">' + line
+//                                    + '</span>' + $(this).html()
+                        })
+                $(this).remove()
+                parentBlock.append('<pre class="block rounded"><code>' + code
+                        + '</code></pre>')
+            });
     $('pre code:not(.hljs)').each(function(i, block) {
-        if (!$(this).hasClass('nohighlight')) { 
+        if (!$(this).hasClass('nohighlight')) {
             $(this).html($.trim($(this).html()));
             hljs.highlightBlock($(this)[0]);
+            // numberLines($(this).parent()[0])
             $(this).parent().show();
         }
     });
     $('pre:not(:has(code))').each(function(i, block) {
+        if ($(this).parent().hasClass('line-code')) {
+            return;
+        }
         $(this).html($.trim($(this).html()));
         $(this).addClass('addPadding')
-        $(this).show()
     });
     $('pre').show();
 }
--- lib/hooks/view_highlighted_hook.rb
+++ lib/hooks/view_highlighted_hook.rb
@@ -4,10 +4,12 @@ module Highlightjs
       def view_layouts_base_html_head(context={})
         setting = Setting.plugin_redmine_highlightjs[:theme]
         setting = 'monokai_sublime' if setting.nil? || setting.empty?
-        return stylesheet_link_tag("#{setting}.css", :plugin => "redmine_highlightjs", :media => "screen") +
-        stylesheet_link_tag("fixes.css", :plugin => "redmine_highlightjs", :media => "screen") +
-        javascript_include_tag('highlight.pack.min.js', :plugin => 'redmine_highlightjs') +
-        javascript_include_tag('loader.js', :plugin => 'redmine_highlightjs')
+        return stylesheet_link_tag("themes/#{setting}.css", :plugin => "redmine_highlightjs", :media => "screen") +
+#          stylesheet_link_tag("numberlines.css", :plugin => "redmine_highlightjs", :media => "screen") +
+          stylesheet_link_tag("fixes.css", :plugin => "redmine_highlightjs", :media => "screen") +
+          javascript_include_tag('highlight.pack.min.js', :plugin => 'redmine_highlightjs') +
+#          javascript_include_tag('numberlines.js', :plugin => 'redmine_highlightjs') +
+          javascript_include_tag('loader.js', :plugin => 'redmine_highlightjs')
       end
     end
   end
