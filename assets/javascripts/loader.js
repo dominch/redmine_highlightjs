d8e819156e615c799d674f2f2759e461a71a75e8
Dominik Chmaj <dominik.chmaj@nowystylgroup.com>
2015-09-15 12:34:08 +0200

    task #3: No highlight in view-source mode
    Task-Url: http://github.com/dominch/redmine_highlightjs/issues/issue/3
    Signed-off-by: Dominik Chmaj <dominik.chmaj@nowystylgroup.com>

--- assets/javascripts/loader.js
+++ assets/javascripts/loader.js
@@ -1,29 +1,32 @@
-function updateCodes() {
+function updateCodes(file_class) {
+
     $('table.syntaxhl th.line-num').remove();
     $('table.syntaxhl').each(
             function(i, block) {
                 var parentBlock = $(this).parent()
                 var code = '';
                 var line = 0;
-                $(this).find('td pre').each(
-                        function(i, block) {
-                            line++;
+                $(this).find('td pre').each(function(i, block) {
+                    line++;
 
-                            var html = $(this).html()
-                            if (!html.trim()) html = "\n"
-                            code += html
-//                            code += '<span class="lineNumber">' + line
-//                                    + '</span>' + $(this).html()
-                        })
+                    var html = $(this).html()
+                    if (!html.trim())
+                        html = "\n"
+                    code += html
+                })
                 $(this).remove()
-                parentBlock.append('<pre class="block rounded"><code>' + code
-                        + '</code></pre>')
+                if (file_class) {
+                    var header = '<pre class="block rounded"><code class="'
+                            + file_class + '">'
+                } else {
+                    var header = '<pre class="block rounded"><code>';
+                }
+                parentBlock.append(header + code + '</code></pre>')
             });
     $('pre code:not(.hljs)').each(function(i, block) {
         if (!$(this).hasClass('nohighlight')) {
             $(this).html($.trim($(this).html()));
             hljs.highlightBlock($(this)[0]);
-            // numberLines($(this).parent()[0])
             $(this).parent().show();
         }
     });
@@ -38,9 +41,15 @@ function updateCodes() {
 }
 
 $(document).ready(function() {
-    updateCodes();
+
+    var file_class = ''
+    if ($('body').hasClass('controller-attachments')) {
+        file_class = document.location.toString().split('.').pop()
+    }
+
+    updateCodes(file_class);
     setInterval(function() {
-        updateCodes();
+        updateCodes(file_class);
     }, 5000);
     $(document).ajaxComplete(function() {
         updateCodes();
