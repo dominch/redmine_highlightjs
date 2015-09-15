044144ebdf9fd9e8ff68524f049d4a87cdf29fbb
Dominik Chmaj <dominik.chmaj@gmail.com>
2015-09-15 12:06:38 +0200

    task #9: Code block disappeared in IE8
    Task-Url: http://github.com/dominch/redmine_highlightjs/issues/issue/9
    Signed-off-by: Dominik Chmaj <dominik.chmaj@gmail.com>

--- lib/highlightjs_highlighting.rb
+++ lib/highlightjs_highlighting.rb
@@ -1,11 +1,11 @@
 module HighlightjsHighlighting
   class << self
     def highlight_by_filename(text, filename)
-      Rails.logger.warn "redmine_highlightjs: syntax by filename #{filename}"
+      Rails.logger.info "redmine_highlightjs: syntax by filename #{filename}"
     end
 
     def highlight_by_language(text, language)
-      Rails.logger.warn "redmine_highlightjs: syntax #{language}"
+      Rails.logger.info "redmine_highlightjs: syntax #{language}"
     end
   end
 end
--- lib/hooks/view_highlighted_hook.rb
+++ lib/hooks/view_highlighted_hook.rb
@@ -2,16 +2,38 @@ module Highlightjs
   module Hooks
     class ViewHighlightedHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        setting = Setting.plugin_redmine_highlightjs[:theme]
-        setting = 'monokai_sublime' if setting.nil? || setting.empty?
-        return stylesheet_link_tag("themes/#{setting}.css", :plugin => "redmine_highlightjs", :media => "screen") +
-#          stylesheet_link_tag("numberlines.css", :plugin => "redmine_highlightjs", :media => "screen") +
+
+        request = context[:request].env['HTTP_USER_AGENT']
+        user_agent = UserAgent.parse(request)
+        
+        if SupportedBrowsers.detect { |browser| user_agent >= browser }
+          Rails.logger.info "redmine_highlightjs2: supported browser: #{user_agent}"
+          Redmine::SyntaxHighlighting.highlighter = 'Highlightjs'
+          setting = Setting.plugin_redmine_highlightjs[:theme]
+          setting = 'monokai_sublime' if setting.nil? || setting.empty?
+          return stylesheet_link_tag("themes/#{setting}.css", :plugin => "redmine_highlightjs", :media => "screen") +
           stylesheet_link_tag("fixes.css", :plugin => "redmine_highlightjs", :media => "screen") +
           javascript_include_tag('highlight.pack.min.js', :plugin => 'redmine_highlightjs') +
-#          javascript_include_tag('numberlines.js', :plugin => 'redmine_highlightjs') +
           javascript_include_tag('loader.js', :plugin => 'redmine_highlightjs')
+        else
+          Rails.logger.info "redmine_highlightjs2: *NOT* supported browser: #{user_agent}"
+          return '';
+        end
+
       end
     end
+
+    private
+
+    Browser = Struct.new(:browser, :version)
+
+    SupportedBrowsers = [
+      Browser.new('Safari', '2'),
+      Browser.new('Firefox', '3'),
+      Browser.new('Chrome', '10'),
+      Browser.new('Internet Explorer', '9')
+    ]
+
   end
 end
 
